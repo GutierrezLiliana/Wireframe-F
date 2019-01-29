c32dfe880aa9efe484309f023999bd9be78dd583
GutierrezLiliana <liliana_gutierrez@davincischools.org>
2019-01-29 15:46:07 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function addClass(el){
 function removeClass(el){
 	el.classList.remove("pats-silver");
 }
+
+function beach(el){
+	document.getElementById("one").src = ("images/beach1.jpg");
+	document.getElementById("two").src = ("images/beach2.jpg");
+	document.getElementById("three").src = ("images/beach3.jpg");
+	document.getElementById("four").src = ("images/beach4.jpg");
+	document.getElementById("one").classList.toggle("flower3");
+	document.getElementById("two").classList.toggle("beach2");
+}
